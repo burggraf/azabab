299b41f4014561e16617b064bdb27dc45fda2728
Mark Burggraf <markb@mantisbible.com>
2023-12-13 16:04:42 -0800

    change permssions on log files

--- server/files/stats.sh
+++ server/files/stats.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 # stats.sh <container_name> <event_name>
 # i.e. stats.sh container123 start
+FILE="/home/ubuntu/stats/$(date -u '+%s').txt"
+
 sudo docker stats \
 	--no-stream \
 	--format "$(date -u '+%s')\t$2\t{{.Name}}\t{{.CPUPerc}}\t{{.MemUsage}}\t{{.NetIO}}\t{{.BlockIO}}" \
 	$1 \
-	| sed 's| / |\t|g' > /home/ubuntu/stats/$(date -u '+%s').txt
+	| sed 's| / |\t|g' \
+	| grep -v 'ssh-server' > $FILE
 
-
-
+# Change file permissions to read and write for everyone
+chmod 666 $FILE
