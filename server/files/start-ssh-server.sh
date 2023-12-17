8a2870d0dad31a2f140edc421b27114757a2f247
Mark Burggraf <markb@mantisbible.com>
2023-12-17 08:02:18 -0800

    removed ssh-server.service, added start-ssh-server.sh to run the ssh-server container in restart mode

--- server/files/start-ssh-server.sh
+++ server/files/start-ssh-server.sh
@@ -1,3 +1,19 @@
-sudo docker run -d -p 2222:22 \
--v /home/ubuntu/data:/data \
---name ssh-server ssh-server
+#!/bin/bash
+
+CONTAINER_NAME="ssh-server"
+
+# Check if the container exists
+if sudo /usr/bin/docker ps -a --format '{{.Names}}' | grep -q "^${CONTAINER_NAME}$"; then
+    echo "Container ${CONTAINER_NAME} exists."
+
+    # Check if the container is not running
+    if ! sudo /usr/bin/docker ps --format '{{.Names}}' | grep -q "^${CONTAINER_NAME}$"; then
+        echo "Starting existing container ${CONTAINER_NAME}."
+        sudo /usr/bin/docker start "${CONTAINER_NAME}"
+    else
+        echo "Container ${CONTAINER_NAME} is already running."
+    fi
+else
+    echo "Container ${CONTAINER_NAME} does not exist. Running a new container."
+    sudo /usr/bin/docker run --restart=unless-stopped --detach -p 2222:22 -v /home/ubuntu/data:/data --name "${CONTAINER_NAME}" ssh-server
+fi
