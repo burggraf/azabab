792116d35ed193998e3e2578e09764e4f8076a41
Mark Burggraf <markb@mantisbible.com>
2023-12-18 21:16:45 -0800

    new method of starting docker containers using rust -- MUCH faster

--- server/rust-server/src/catch_all_handler.rs
+++ server/rust-server/src/catch_all_handler.rs
@@ -1,26 +1,77 @@
-use hyper::{Request, Response, Body, StatusCode, client::Client};
-use std::process::Command;
+// use hyper::{Request, Response, Body, StatusCode, client::Client};
+// use std::process::Command;
+use hyper::{Body, Client, Request, Response, Uri};
+
+//use shiplift::{ContainerOptions, Docker, PullOptions, builder::VolumeBind};
+// use tokio::time::{sleep, Duration};
+use std::env;
+use shiplift::{ContainerOptions, Docker};
+use tokio::time::{interval, Duration};
+//use shiplift::rep::ContainerDetails;
 
 pub async fn handle_catch_all(mut req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
-            // Clone header values before mutating `req`
-            let original_port = req.headers().get("X-Original-Port").unwrap().to_str().unwrap().to_owned();
-            let original_uri = req.headers().get("X-Original-URI").unwrap().to_str().unwrap().to_owned();
+            // Clone header values before mutating `req`                
+            // let original_port = req.headers().get("X-Original-Port").unwrap().to_str().unwrap().to_owned();
+            // let original_uri = req.headers().get("X-Original-URI").unwrap().to_str().unwrap().to_owned();
+            let original_port = req.headers().get("X-Original-Port").unwrap().to_str().unwrap().to_string();
+            let original_uri = req.headers().get("X-Original-URI").unwrap().to_str().unwrap().to_string();
+        
 
-            // Run command
-            let output = Command::new("sudo")
-                .arg("/home/ubuntu/run.sh")
-                .arg(&original_port)
-                .output()
-                .expect("failed to execute process");
+                // Docker interaction
+    let docker = Docker::new();
+    let current_dir = env::current_dir().unwrap();
+    let volume_mounts = vec![
+        format!("{}:/home/pocketbase/pb_data", current_dir.join(format!("data/{}/pb_data", original_port)).display()),
+        format!("{}:/home/pocketbase/pb_public", current_dir.join(format!("data/{}/pb_public", original_port)).display()),
+        format!("{}:/home/pocketbase/pb_migrations", current_dir.join(format!("data/{}/pb_migrations", original_port)).display()),
+        format!("{}:/home/pocketbase/pb_hooks", current_dir.join(format!("data/{}/pb_hooks", original_port)).display()),
+    ];
+    let container_options = ContainerOptions::builder("pbdocker")
+    .name(&original_port)
+    .cpus(0.25)
+    .memory(96 * 1024 * 1024) // Memory in bytes
+    .expose(8090, "tcp", original_port.parse::<u32>().unwrap())
+    .volumes(volume_mounts.iter().map(AsRef::as_ref).collect::<Vec<&str>>())
+    .auto_remove(true)
+    .build();
 
-            let output_str = String::from_utf8_lossy(&output.stdout);
+    let container = docker.containers().create(&container_options).await.unwrap();
+    let _ = docker.containers().get(&container.id).start().await.unwrap();
 
-            if output_str.trim() != "OK" {
-                return Ok(Response::builder()
-                    .status(StatusCode::INTERNAL_SERVER_ERROR)
-                    .body(Body::from("Error in executing command"))
-                    .unwrap());
-            } 
+    // Optional: Check container's status or wait for it to be ready
+    // e.g., sleep(Duration::from_millis(500)).await;
+
+    //let server_uri = format!("http://localhost:8090/invalid-page");
+    let server_uri = format!("http://localhost:{}/invalid-page", original_port);
+
+    let client = Client::new();
+    let mut interval = interval(Duration::from_millis(50));
+    let mut server_ready = false;
+    
+    for _ in 0..200 { // attempts 20 times at a 500ms interval
+        interval.tick().await;
+        match client.get(Uri::try_from(server_uri.as_str()).unwrap()).await {
+            Ok(response) => {
+                if response.status() == hyper::StatusCode::NOT_FOUND {
+                    // Server is up and responding with 404 for the invalid page
+                    server_ready = true;
+                    break;
+                }
+            }
+            Err(_e) => {
+                // Handle connection errors (e.g., server not ready yet)
+                //eprintln!("Error connecting to server: {}", e);
+            }
+        }
+    }
+    
+    if !server_ready {
+        eprintln!("Server did not start in expected time");
+        // Handle this situation appropriately
+    }
+
+
+
 
             let method = req.method().clone();
             let headers = req.headers().clone();
