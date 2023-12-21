5b5dc0a3e48d38828554e7d53d702839a26b1ae5
Mark Burggraf <markb@mantisbible.com>
2023-12-21 07:28:02 -0800

    formatting gui accordion component

--- src/routes/project/[id]/AccordionComponent.svelte
+++ src/routes/project/[id]/AccordionComponent.svelte
@@ -1,11 +1,39 @@
 <script lang="ts">
+	import { codeWorkingOutline, easelOutline, folderOpenOutline, hammerOutline, personOutline, serverOutline } from 'ionicons/icons'
     import AccordionWrapper from './AccordionWrapper.svelte';
     export let node: any;
     export let paddingLeft = 0;
+    const getColor = (paddingLeft: number) => {
+        switch (paddingLeft) {
+            case 0:
+                return 'dark';
+            case 20:
+                return 'medium';
+            case 40:
+                return 'light';
+            default:
+                return 'light';
+        }
+    };
+    const getIcon = (title: string, children: number) => {
+        switch (title) {
+            case 'pb_data':
+                return serverOutline;
+            case 'pb_migrations':
+                return codeWorkingOutline;
+            case 'pb_public':
+                return easelOutline;
+            case 'pb_hooks':
+                return hammerOutline;
+            default:
+                return children > 0 ? folderOpenOutline : null;
+        }
+    };
   </script>
   
   <ion-accordion value={node.title}>
-    <ion-item slot="header" color="light" style={`padding-left: ${paddingLeft}px;`}>
+    <ion-item slot="header" color={getColor(paddingLeft)} style={`padding-left: ${paddingLeft}px;`}>
+      <ion-icon slot="start" icon={getIcon(node.title, node.children?.length)} />
       <ion-label>{node.title}</ion-label>
     </ion-item>
   
