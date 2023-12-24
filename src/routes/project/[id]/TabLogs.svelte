6f1864aae2277853098adc2c95a5cc7e4e272aa2
Mark Burggraf <markb@mantisbible.com>
2023-12-23 18:23:06 -0800

    show instance logs

--- src/routes/project/[id]/TabLogs.svelte
+++ src/routes/project/[id]/TabLogs.svelte
@@ -2,8 +2,6 @@
     let stats: any = []
     import type { ProjectInstance } from './interfaces'
     import { currentUser, pb } from '$services/backend.service'
-    import moment from 'moment'
-	const formatDate = (timestamp: number) => moment(timestamp * 1000).format('MM/DD/YY HH:mm:ss')
     
     export let project_instances: ProjectInstance[] = [
 		{
@@ -16,56 +14,31 @@
 			site_id: '',
 			type: 'primary',
 		},
-	]
+	];
 
     const loadData = async () => {
-        const resultList = await pb.collection('stats_view').getList(1, 50, {
-            filter: `instance_id = "${project_instances[0].id}"`,
-            columns: `ts, event, cpu_usage, mem_usage, disk_read, disk_write, net_in, net_out`,
-            sort: '-ts',
-        })
-        console.log('loadData: resultList', resultList)   
-        stats = resultList.items
+        const { data, error } = await pb.send(`/getinstancefile`, {
+				method: 'POST',
+				body: {
+					project_instance_id: project_instances[0].id,
+					path: 'pb_data/log.txt',
+				},
+			})
+			console.log('*** getinstancefile: data, error', data, error)
+			if (data?.raw) {
+				console.log('data.raw', data.raw)
+                const el = document.getElementById('logviewer');                
+                if (el) { 
+                    el.innerText = data.raw; 
+                }
+			} 
+
     }
     setTimeout(() => {
         loadData()
-    }, 1000)
-    // loadData();
+    }, 500)
 
 </script>
 <div class="ion-padding" style="overflow: scroll;height: 100%">
-    <!-- <ion-segment on:ionChange={changeMetrics} id="metrics" value="info">
-        <ion-segment-button value="info">
-            <ion-label>Info</ion-label>
-        </ion-segment-button>
-        {#each project_instances as project_instance, index}
-            <ion-segment-button value={project_instance.id}>
-                <ion-label>{project_instance.site_name}</ion-label>
-            </ion-segment-button>
-        {/each}
-    </ion-segment> -->
-    <ion-grid class="ion-padding" id="statGrid" style="width: 100%">
-        <ion-row style="color:var(--ion-color-light);background-color:var(--ion-color-medium)">
-            <ion-col>Date</ion-col>
-            <ion-col>Event</ion-col>
-            <ion-col>CPU%</ion-col>
-            <ion-col>Mem(mb)</ion-col>
-            <ion-col>Disk Read</ion-col>
-            <ion-col>Disk Write</ion-col>
-            <ion-col>Net In</ion-col>
-            <ion-col>Net Out</ion-col>
-        </ion-row>
-        {#each stats as stat, index}
-            <ion-row>
-                <ion-col>{formatDate(stat.ts)}</ion-col>
-                <ion-col>{stat.event}</ion-col>
-                <ion-col>{stat.cpu_usage}</ion-col>
-                <ion-col>{stat.mem_usage}</ion-col>
-                <ion-col>{stat.disk_read}</ion-col>
-                <ion-col>{stat.disk_write}</ion-col>
-                <ion-col>{stat.net_in}</ion-col>
-                <ion-col>{stat.net_out}</ion-col>
-            </ion-row>
-        {/each}
-    </ion-grid>
+    <pre id="logviewer"></pre>
 </div>
