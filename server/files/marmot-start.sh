61a1b8a076484a3a6fdf42a20c43e839c9dcc4fb
Mark Burggraf <markb@mantisbible.com>
2023-12-26 06:03:30 -0800

    updated marmot version

--- server/files/marmot-start.sh
+++ server/files/marmot-start.sh
@@ -1,1 +1,3 @@
-sudo ./marmot -config config.toml -cluster-addr west-2.azabab.com:4002 -cluster-peers 'nats://west-1.azabab.com:4001/','nats://west-3.azabab.com:4003'
+sudo ./marmot -config config.toml \
+    -cluster-addr west-2.azabab.com:4002 \
+    -cluster-peers 'nats://west-1.azabab.com:4001/','nats://west-3.azabab.com:4003'
