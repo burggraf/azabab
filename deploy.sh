b702d8d4e987e622f8976942aa3bc42d595a3dd5
Mark Burggraf <markb@mantisbible.com>
2024-01-02 10:31:43 -0800

    change icon sizes

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,6 @@
 # init with:  wrangler pages project create
 npm run build 
 # && wrangler pages deploy --project-name azabab ./build
+scp -P 2222 -r build/* app@east-3.azabab.com:/pb_public
+scp -P 2222 -r build/* app@east-4.azabab.com:/pb_public
+scp -P 2222 -r build/* app@west-1.azabab.com:/pb_public
