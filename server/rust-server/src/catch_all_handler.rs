03d170c46440e52e9ebf1387e9e4bc82b5a732b0
Mark Burggraf <markb@mantisbible.com>
2024-01-03 09:54:20 -0800

    add ENVIRONMENT to docker containers

--- server/rust-server/src/catch_all_handler.rs
+++ server/rust-server/src/catch_all_handler.rs
@@ -1,25 +1,15 @@
-// use hyper::{Request, Response, Body, StatusCode, client::Client};
-// use std::process::Command;
 use hyper::{Body, Client, Request, Response, Uri, body::to_bytes};
-
-//use shiplift::{ContainerOptions, Docker, PullOptions, builder::VolumeBind};
-// use tokio::time::{sleep, Duration};
-//use std::env;
 use shiplift::{ContainerOptions, Docker};
 use tokio::time::{interval, Duration};
-//use shiplift::rep::ContainerDetails;
-
 use serde_json::Value; // Add serde_json to your Cargo.toml dependencies
-
+use std::env; // Import the env module
 
 pub async fn handle_catch_all(mut req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
-            // Clone header values before mutating `req`                
-            // let original_port = req.headers().get("X-Original-Port").unwrap().to_str().unwrap().to_owned();
-            // let original_uri = req.headers().get("X-Original-URI").unwrap().to_str().unwrap().to_owned();
-            let original_port = req.headers().get("X-Original-Port").unwrap().to_str().unwrap().to_string();
-            let original_uri = req.headers().get("X-Original-URI").unwrap().to_str().unwrap().to_string();
+    // Clone header values before mutating `req`
+    let original_port = req.headers().get("X-Original-Port").unwrap().to_str().unwrap().to_string();
+    let original_uri = req.headers().get("X-Original-URI").unwrap().to_str().unwrap().to_string();
 
-                // Docker interaction
+     // Docker interaction
     let docker = Docker::new();
     //let current_dir = env::current_dir().unwrap();
     let base_path = "/home/ubuntu";
@@ -30,22 +20,21 @@ pub async fn handle_catch_all(mut req: Request<Body>) -> Result<Response<Body>, 
         format!("{}/data/{}/pb_hooks:/home/pocketbase/pb_hooks", base_path, original_port),
         format!("{}/data/{}/marmot:/marmot", base_path, original_port),
     ];
+    // Retrieve the ENVIRONMENT variable from the Rust application's environment
+    let environment_variable = env::var("ENVIRONMENT").unwrap_or_else(|_| "default_value".to_string());
     let container_options = ContainerOptions::builder("pbdocker")
-    .name(&original_port)
-    .cpus(0.50)
-    .memory(256 * 1024 * 1024) // Memory in bytes
-    .expose(8090, "tcp", original_port.parse::<u32>().unwrap())
-    .volumes(volume_mounts.iter().map(AsRef::as_ref).collect::<Vec<&str>>())
-    .auto_remove(true)
-    .build();
-
+        .name(&original_port)
+        .cpus(0.50)
+        .memory(256 * 1024 * 1024) // Memory in bytes
+        .expose(8090, "tcp", original_port.parse::<u32>().unwrap())
+        .volumes(volume_mounts.iter().map(AsRef::as_ref).collect::<Vec<&str>>())
+        .env(vec![&format!("ENVIRONMENT={}", environment_variable)]) // Set the environment variable
+        .auto_remove(true)
+        .build();
+  
     let container = docker.containers().create(&container_options).await.unwrap();
     let _ = docker.containers().get(&container.id).start().await.unwrap();
 
-    // Optional: Check container's status or wait for it to be ready
-    // e.g., sleep(Duration::from_millis(500)).await;
-
-    //let server_uri = format!("http://localhost:8090/invalid-page");
     let server_uri = format!("http://localhost:{}/api/health", original_port);
     /*
     {
@@ -85,33 +74,29 @@ pub async fn handle_catch_all(mut req: Request<Body>) -> Result<Response<Body>, 
         eprintln!("Server did not start in expected time");
         // Handle this situation appropriately
     }
+    let method = req.method().clone();
+    let headers = req.headers().clone();
 
+    let entire_body = hyper::body::to_bytes(req.body_mut()).await?;
 
+    // Create a client and make a request to the original_uri
+    let client = Client::new();
+    let forward_uri = format!("http://localhost:{}{}", &original_port, &original_uri);
 
+    let mut forward_req_builder = Request::builder()
+        .method(method)
+        .uri(forward_uri);
 
-            let method = req.method().clone();
-            let headers = req.headers().clone();
+    // Apply the cloned headers to the new request
+    for (key, value) in headers.iter() {
+        forward_req_builder = forward_req_builder.header(key, value);
+    }
 
-            let entire_body = hyper::body::to_bytes(req.body_mut()).await?;
+    let forward_req = forward_req_builder
+        .body(Body::from(entire_body))
+        .unwrap();
 
-            // Create a client and make a request to the original_uri
-            let client = Client::new();
-            let forward_uri = format!("http://localhost:{}{}", &original_port, &original_uri);
+    let resp = client.request(forward_req).await?;
 
-            let mut forward_req_builder = Request::builder()
-                .method(method)
-                .uri(forward_uri);
-
-            // Apply the cloned headers to the new request
-            for (key, value) in headers.iter() {
-                forward_req_builder = forward_req_builder.header(key, value);
-            }
-
-            let forward_req = forward_req_builder
-                .body(Body::from(entire_body))
-                .unwrap();
-
-            let resp = client.request(forward_req).await?;
-
-            Ok(resp)
+    Ok(resp)
 }
