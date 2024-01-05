b8d6189402ceffd30d05a34576be1440a23369e3
Mark Burggraf <markb@mantisbible.com>
2024-01-04 18:43:32 -0800

    send pb version to container

--- server/files/start.sh
+++ server/files/start.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-/home/pocketbase/pocketbase serve --http 0.0.0.0:8090 >> pb_data/log.txt 2>&1 &
+#/home/pocketbase/pocketbase serve --http 0.0.0.0:8090 >> pb_data/log.txt 2>&1 &
+# /exe is mapped to the current version passed in the header
+/exe/pocketbase serve --http 0.0.0.0:8090 --dir pb_data --hooksDir pb_hooks --migrationsDir pb_migrations --publicDir pb_public >> pb_data/log.txt 2>&1 &
 if [ -f /marmot/marmot.toml ]; then
     /home/pocketbase/marmot -config /marmot/marmot.toml >> pb_data/sync-log.txt 2>&1 &
 fi
--- server/upgrade.sh
+++ server/upgrade.sh
@@ -6,33 +6,5 @@ if [ -z "$1" ] || [ -z "$2" ]
     echo "example: ./install.sh west-1.azabab.com azabab.com"
     exit 1
 fi
-scp -r files/nginx.conf ubuntu@$1:~/
-
-ssh ubuntu@$1 "sudo touch /etc/nginx/domain_pb_version.txt"
-ssh ubuntu@$1 "sudo chmod 644 /etc/nginx/domain_pb_version.txt"
-ssh ubuntu@$1 "sudo chown ubuntu:ubuntu /etc/nginx/domain_pb_version.txt"
-
-# Replace [DOMAIN] with the provided domain
-ssh ubuntu@$1 "sed -i \"s/\[DOMAIN\]/$2/g\" ~/nginx.conf"
-# Replace [DOMAIN-WITH-DOTS-ESCAPED] with the escaped domain
-#ssh ubuntu@$1 "ESCAPED_DOMAIN=\$(echo $2 | sed 's/\./\\./g');sed -i \"s/\[DOMAIN-WITH-DOTS-ESCAPED\]/$ESCAPED_DOMAIN/g\" ~/nginx.conf"
-# Escape dots in the domain for the second replacement
-ESCAPED_DOMAIN=$(echo $2 | sed 's/\./\\\\./g')
-#ssh ubuntu@$1 'ESCAPED_DOMAIN=$(echo '"$2"' | sed "s/\./\\\./g"); sed -i "s/\[DOMAIN-WITH-DOTS-ESCAPED\]/$ESCAPED_DOMAIN/g" ~/nginx.conf'
-ssh ubuntu@$1 "sed -i 's/\[DOMAIN-WITH-DOTS-ESCAPED\]/$ESCAPED_DOMAIN/g' ~/nginx.conf"
-
-ssh ubuntu@$1 "sudo mv ~/nginx.conf /etc/nginx"
-ssh ubuntu@$1 "sudo chmod 644 /etc/nginx/nginx.conf"
-ssh ubuntu@$1 "sudo chown root:root /etc/nginx/nginx.conf"
-
-echo ""
-echo "Test nginx"
-echo ""
-# ssh ubuntu@$1 "sudo kill -HUP $(cat /var/run/nginx.pid)"
-ssh ubuntu@$1  "sudo nginx -t"
-
-echo ""
-echo "Reload nginx"
-echo ""
-# ssh ubuntu@$1 "sudo kill -HUP $(cat /var/run/nginx.pid)"
-ssh ubuntu@$1  "sudo kill -HUP \$(cat /var/run/nginx.pid)"
+scp files/start.sh ubuntu@$1:~
+ssh ubuntu@$1 "sudo ./build.sh"
