9ff3f57363659d26cdbbbbe60c4f7aa4b408987d
Mark Burggraf <markb@mantisbible.com>
2024-01-13 16:48:34 -0800

    updated install for haproxy

--- server/install.sh
+++ server/install.sh
@@ -48,7 +48,7 @@ echo ""
 echo "Request Wildcard SSL Certificate"
 echo ""
 scp ./private/cloudflare.ini ubuntu@$1:~
-ssh ubuntu@$1 "sudo certbot certonly --non-interactive --email support@$1 --agree-tos --dns-cloudflare --dns-cloudflare-credentials ./cloudflare.ini -d \"*.azabab.com\" -d \"*.$2\""
+ssh ubuntu@$1 "sudo certbot certonly --non-interactive --email support@azabab.com --agree-tos --dns-cloudflare --dns-cloudflare-credentials ./cloudflare.ini -d \"*.azabab.com\" -d \"*.$2\""
 
 echo ""
 echo "Copy all necessary files to the server"
@@ -64,41 +64,45 @@ echo "*** Generate rsa key pair (if necessary) ***"
 echo ""
 ssh ubuntu@$1 "/home/ubuntu/generate_ssh_key.sh"
 
-echo ""
-echo "Configuring Nginx"
-echo ""
-ssh ubuntu@$1 "sudo mv ~/options-ssl-nginx.conf /etc/letsencrypt"
-ssh ubuntu@$1 "sudo chmod 644 /etc/letsencrypt/options-ssl-nginx.conf"
-ssh ubuntu@$1 "sudo chown root:root /etc/letsencrypt/options-ssl-nginx.conf"
-ssh ubuntu@$1 "sudo touch /etc/nginx/domain_ports.txt"
-ssh ubuntu@$1 "sudo chmod 644 /etc/nginx/domain_ports.txt"
-ssh ubuntu@$1 "sudo chown ubuntu:ubuntu /etc/nginx/domain_ports.txt"
+# echo ""
+# echo "Configuring Nginx"
+# echo ""
+# ssh ubuntu@$1 "sudo mv ~/options-ssl-nginx.conf /etc/letsencrypt"
+# ssh ubuntu@$1 "sudo chmod 644 /etc/letsencrypt/options-ssl-nginx.conf"
+# ssh ubuntu@$1 "sudo chown root:root /etc/letsencrypt/options-ssl-nginx.conf"
+# ssh ubuntu@$1 "sudo touch /etc/nginx/domain_ports.txt"
+# ssh ubuntu@$1 "sudo chmod 644 /etc/nginx/domain_ports.txt"
+# ssh ubuntu@$1 "sudo chown ubuntu:ubuntu /etc/nginx/domain_ports.txt"
 
-ssh ubuntu@$1 "sudo touch /etc/nginx/domain_pb_version.txt"
-ssh ubuntu@$1 "sudo chmod 644 /etc/nginx/domain_pb_version.txt"
-ssh ubuntu@$1 "sudo chown ubuntu:ubuntu /etc/nginx/domain_pb_version.txt"
+# ssh ubuntu@$1 "sudo touch /etc/nginx/domain_pb_version.txt"
+# ssh ubuntu@$1 "sudo chmod 644 /etc/nginx/domain_pb_version.txt"
+# ssh ubuntu@$1 "sudo chown ubuntu:ubuntu /etc/nginx/domain_pb_version.txt"
 
 # Escape dots in the domain for the second replacement
 
 # Replace [DOMAIN] with the provided domain
-ssh ubuntu@$1 "sed -i \"s/\[DOMAIN\]/$2/g\" ~/nginx.conf"
+### ssh ubuntu@$1 "sed -i \"s/\[DOMAIN\]/$2/g\" ~/nginx.conf"
+
 # Replace [DOMAIN-WITH-DOTS-ESCAPED] with the escaped domain
 #ssh ubuntu@$1 "ESCAPED_DOMAIN=\$(echo $2 | sed 's/\./\\./g');sed -i \"s/\[DOMAIN-WITH-DOTS-ESCAPED\]/$ESCAPED_DOMAIN/g\" ~/nginx.conf"
 # Escape dots in the domain for the second replacement
-ESCAPED_DOMAIN=$(echo $2 | sed 's/\./\\\\./g')
+###ESCAPED_DOMAIN=$(echo $2 | sed 's/\./\\\\./g')
 #ssh ubuntu@$1 'ESCAPED_DOMAIN=$(echo '"$2"' | sed "s/\./\\\./g"); sed -i "s/\[DOMAIN-WITH-DOTS-ESCAPED\]/$ESCAPED_DOMAIN/g" ~/nginx.conf'
-ssh ubuntu@$1 "sed -i 's/\[DOMAIN-WITH-DOTS-ESCAPED\]/$ESCAPED_DOMAIN/g' ~/nginx.conf"
+###ssh ubuntu@$1 "sed -i 's/\[DOMAIN-WITH-DOTS-ESCAPED\]/$ESCAPED_DOMAIN/g' ~/nginx.conf"
 
-ssh ubuntu@$1 "sudo mv ~/nginx.conf /etc/nginx"
-ssh ubuntu@$1 "sudo chmod 644 /etc/nginx/nginx.conf"
-ssh ubuntu@$1 "sudo chown root:root /etc/nginx/nginx.conf"
+# ssh ubuntu@$1 "sudo mv ~/nginx.conf /etc/nginx"
+# ssh ubuntu@$1 "sudo chmod 644 /etc/nginx/nginx.conf"
+# ssh ubuntu@$1 "sudo chown root:root /etc/nginx/nginx.conf"
 
 echo ""
 echo "Add cron job to renew SSL certificate"
 echo "Add cron job to stop inactive containers (inactive for 5 minutes)"
 echo "Add cron job to collect container stats every minute"
 echo ""
-ssh ubuntu@$1 "echo \"0 */12 * * * sudo /usr/bin/certbot renew --quiet\" | sudo tee -a /var/spool/cron/crontabs/root"
+# create the combined cert file for haproxy after renewing the cert
+# sudo cat /etc/letsencrypt/live/azabab.com/fullchain.pem /etc/letsencrypt/live/azabab.com/privkey.pem > /home/ubuntu/proxy-server/haproxy_cert.pem
+ssh ubuntu@$1 "echo \"0 */12 * * * sudo /usr/bin/certbot renew --quiet\" | sudo tee -a /var/spool/cron/crontabs/root && cat /etc/letsencrypt/live/azabab.com/fullchain.pem /etc/letsencrypt/live/azabab.com/privkey.pem > /home/ubuntu/proxy-server/haproxy_cert.pem"
+
 ssh ubuntu@$1 "echo \"* * * * * /home/ubuntu/stop_inactive_containers.sh\" | sudo tee -a /var/spool/cron/crontabs/root"
 ssh ubuntu@$1 "echo \"* * * * * /home/ubuntu/stats.sh\" | sudo tee -a /var/spool/cron/crontabs/root"
 ssh ubuntu@$1 "mkdir -p /home/ubuntu/stats"
@@ -122,6 +126,23 @@ echo ""
 ssh ubuntu@$1 "cd ~/ssh-server;sudo docker build -t ssh-server ."
 #scp files/start-ssh-server.sh ubuntu@$1:~
 ssh ubuntu@$1 "~/start-ssh-server.sh"
+
+echo ""
+echo "Copy proxy-server files to the server"
+echo ""
+ssh ubuntu@$1 "mkdir ~/proxy-server"
+scp -r proxy-server/* ubuntu@$1:~/proxy-server
+echo "Create the combined cert for haproxy"
+ssh ubuntu@$1 "~/proxy-server/make-cert.sh"
+echo ""
+echo "Install HAProxy"
+ssh ubuntu@$1 "sudo apt install -y haproxy"
+ssh ubuntu@$1 "sudo mv ~/haproxy.service /etc/systemd/system"
+ssh ubuntu@$1 "sudo chmod 644 /etc/systemd/system/haproxy.service"
+ssh ubuntu@$1 "sudo chown root:root /etc/systemd/system/haproxy.service"
+ssh ubuntu@$1 "sudo systemctl enable haproxy.service"
+ssh ubuntu@$1 "sudo systemctl start haproxy.service"
+echo ""
 
 echo ""
 echo "Copy nats-server files to the server"
