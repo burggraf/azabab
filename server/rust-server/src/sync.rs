4e28f4966be922a752d172932639b2add27b6737
Mark Burggraf <markb@mantisbible.com>
2024-01-18 06:10:47 -0800

    added new options to rust-server sync command: folder, plus an array of excludes

--- server/rust-server/src/sync.rs
+++ server/rust-server/src/sync.rs
@@ -1,7 +1,6 @@
 use hyper::{Request, Response, Body, StatusCode};
-use serde_json::json;
-use std::{process::Command};
-use std::env;
+use serde_json::{json, Value};
+use std::{process::Command, env};
 
 use crate::AUTH_TOKEN;
 
@@ -23,45 +22,44 @@ pub async fn handle_sync(mut req: Request<Body>, _auth_token: &str) -> Result<Re
             .unwrap());
     }
 
-    // Parse request body
     let body_bytes = hyper::body::to_bytes(req.body_mut()).await?;
     let body_str = String::from_utf8(body_bytes.to_vec()).unwrap();
-    let body_params: serde_json::Value = serde_json::from_str(&body_str).unwrap();
+    let body_params: Value = serde_json::from_str(&body_str).unwrap();
 
     let port = body_params["port"].as_str().unwrap_or_default();
     let direction = body_params["direction"].as_str().unwrap_or_default();
     let destination = body_params["destination"].as_str().unwrap_or_default();
-    println!("sync - port {}, direction {}, destination {}", port, direction, destination);
 
-    // Set current folder
+    // Get the folder parameter, defaulting to "sync" if not provided
+    let folder = body_params["folder"].as_str().unwrap_or("sync");
+
+    // Initialize an empty vector for excludes
+    let mut excludes = Vec::new();
+    // Check if excludes parameter is provided and is an array
+    if let Some(ex_array) = body_params.get("excludes").and_then(Value::as_array) {
+        // Iterate over the array and collect string values
+        excludes = ex_array.iter()
+                           .filter_map(|e| e.as_str())
+                           .map(|e| format!("--exclude {}", e))
+                           .collect();
+    }
+    let exclude_str = excludes.join(" ");
+
     env::set_current_dir("/home/ubuntu/data").unwrap();
 
     match direction {
-        "up" => {
-            // Execute rclone sync for "up" direction
-            let sync_command = format!("rclone sync {} {}:azabab/sync/{} --exclude marmot/marmot.toml", port, destination, port);
-            println!("sync_command: {}", sync_command);
-            Command::new("sh")
-                .arg("-c")
-                .arg(&sync_command)
-                .output()
-                .expect("Failed to execute sync command");
-            // println!("Sync up executed for port {}", port);
-        },
-        "down" => {
-            // Stop Docker container if it's running
-            let _docker_stop_output = Command::new("docker")
-                .args(&["stop", "-t", "0", port])
-                .output();
+        "up" | "down" => {
+            let sync_command = if direction == "up" {
+                format!("rclone sync {} {}:azabab/{}/{} {} --exclude marmot/marmot.toml", port, destination, folder, port, exclude_str)
+            } else {
+                format!("rclone sync {}:azabab/{}/{} {} {} --exclude marmot/marmot.toml", destination, folder, port, port, exclude_str)
+            };
 
-            let sync_command = format!("rclone sync {}:azabab/sync/{} {} --exclude marmot/marmot.toml", destination, port, port);
-            println!("sync_command: {}", sync_command);
             Command::new("sh")
                 .arg("-c")
                 .arg(&sync_command)
                 .output()
                 .expect("Failed to execute sync command");
-            // println!("Sync down executed for port {}", port);
         },
         "delete" => {
             // New code for "delete" direction
@@ -76,8 +74,7 @@ pub async fn handle_sync(mut req: Request<Body>, _auth_token: &str) -> Result<Re
         },        
         _ => println!("Invalid direction: {} for port {}", direction, port),
     }
-    println!("Sync operation complete");
-    // Return success response
+
     Ok(Response::builder()
         .status(StatusCode::OK)
         .body(Body::from(json!({ "data": "Sync operation complete", "error": null }).to_string()))
