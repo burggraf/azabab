164bb6dd729bf361c0657622a891764b66216924
Mark Burggraf <markb@mantisbible.com>
2024-01-21 17:32:03 -0800

    add environment files to pocketbase using /pb_data/.env

--- server/files/start.sh
+++ server/files/start.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 #/home/pocketbase/pocketbase serve --http 0.0.0.0:8090 >> pb_data/log.txt 2>&1 &
 # /exe is mapped to the current version passed in the header
+if [ -f /home/pocketbase/pb_data/.env ]; then
+    source /home/pocketbase/pb_data/.env
+fi
 /exe/pocketbase serve --http 0.0.0.0:8090 --dir pb_data --hooksDir pb_hooks --migrationsDir pb_migrations --publicDir pb_public >> pb_data/log.txt 2>&1 &
 if [ -f /marmot/marmot.toml ]; then
     /home/pocketbase/marmot -config /marmot/marmot.toml >> pb_data/sync-log.txt 2>&1 &
