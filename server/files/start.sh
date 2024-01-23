afb4d79e9aa0059435ffcfe0fc94a39941eeb935
Mark Burggraf <markb@mantisbible.com>
2024-01-22 18:31:40 -0800

    env vars, set HOST env var in every docker container

--- server/files/start.sh
+++ server/files/start.sh
@@ -4,6 +4,7 @@
 if [ -f /home/pocketbase/pb_data/.env ]; then
     source /home/pocketbase/pb_data/.env
 fi
+export HOST="[HOST]"
 /exe/pocketbase serve --http 0.0.0.0:8090 --dir pb_data --hooksDir pb_hooks --migrationsDir pb_migrations --publicDir pb_public >> pb_data/log.txt 2>&1 &
 if [ -f /marmot/marmot.toml ]; then
     /home/pocketbase/marmot -config /marmot/marmot.toml >> pb_data/sync-log.txt 2>&1 &
