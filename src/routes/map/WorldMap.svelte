adda6d9ca7b3fd73feda3b6c9b960a839ecbf72b
Mark Burggraf <markb@mantisbible.com>
2024-01-28 08:34:26 -0800

    updated map

--- src/routes/map/WorldMap.svelte
+++ src/routes/map/WorldMap.svelte
@@ -1,21 +1,24 @@
 <script lang="ts">
 	import { Map, View } from 'ol'
+	// import { Text, Fill } from 'ol/style'; // Import the Text class from ol/style
 	import TileLayer from 'ol/layer/Tile'
+	import { Circle as CircleStyle, Fill, Stroke, Style, Icon } from 'ol/style';	
+	import { Overlay } from 'ol'
+	import Feature from 'ol/Feature';
+	import Point from 'ol/geom/Point';
+	import { fromLonLat } from 'ol/proj';	
 	import OSM from 'ol/source/OSM'
 	import { Vector as VectorLayer } from 'ol/layer'
 	import { Vector as VectorSource } from 'ol/source'
-	import { Point } from 'ol/geom'
-	import { Feature } from 'ol'
-	import { Style, Icon } from 'ol/style'
 	import IonPage from '$ionpage'
-	import { add } from 'ol/coordinate'
-	import { fromLonLat } from 'ol/proj'
-	import { Attribution, defaults as defaultControls } from 'ol/control'
-
+	// import { add } from 'ol/coordinate'
+	import { pb, currentUser } from '$services/backend.service'
 	let map: any
 	let vectorSource: any // Declare vectorSource in the outer scope
 	// Function to initialize the map
-	function initializeMap() {
+	async function initializeMap() {
+		await loadCities()
+		console.log('cities', cities)
 		const mapElement = document.getElementById('map')
 		if (!mapElement) {
 			return
@@ -30,10 +33,6 @@
 		const vectorLayer = new VectorLayer({
 			source: vectorSource,
 		})
-		const attributionControl = new Attribution({
-			className: 'custom-attribution',
-			target: 'my-attribution', // ID of the target element
-		})
 		map = new Map({
 			target: mapElement,
 			layers: [osmLayer, vectorLayer],
@@ -41,100 +40,105 @@
 				center: [0, 0], // Center of the map
 				zoom: 2, // Initial zoom level
 			}),
-			controls: defaultControls({
-				attribution: false, // Disable the default attribution control
-			}).extend([attributionControl]), // Add the custom attribution control
+		})
+		map.addOverlay(tooltipOverlay);
+
+		// Update the tooltip on pointer move
+		map.on('pointermove', (evt: any) => {
+		if (map.hasFeatureAtPixel(evt.pixel)) {
+			const feature = map.getFeaturesAtPixel(evt.pixel)[0];
+			tooltipOverlay.setPosition(evt.coordinate);
+			tooltipElement.innerHTML = feature.get('name');
+			tooltipElement.style.display = '';
+		} else {
+			tooltipElement.style.display = 'none';
+		}
+		});	
+		map.on('pointermove', function(evt: any) {
+			if (map.hasFeatureAtPixel(evt.pixel)) {
+				const feature = map.getFeaturesAtPixel(evt.pixel)[0];
+				const cityName = feature.get('name');
+				// Now you can use cityName to display a tooltip
+				// For example, you could display cityName in a DOM element that serves as a tooltip
+			}
+		});		
+		map.on('click', function(evt: any) {
+			const feature = map.forEachFeatureAtPixel(evt.pixel, function(feature: any) {
+				return feature;
+			});
+			if (feature) {
+				// A feature was clicked, you can now do something with it
+				const cityName = feature.get('name');
+				const cityCode = feature.get('cityCode');
+				console.log('City clicked:', cityName, cityCode);
+			}
+		});
+
+	}
+	let cities: any[] = []
+	const loadCities = async () => {
+		cities = await pb.collection('sites').getFullList({
+			fields: 'metadata, code',
+			sort: 'name',
 		})
 	}
-    const cities = [
-  {
-    "city": "Nuremberg, Germany",
-    "latitude": 49.460983,
-    "longitude": 11.061859
-  },
-  {
-    "city": "Chicago, IL",
-    "latitude": 41.881832,
-    "longitude": -87.623177
-  },
-  {
-    "city": "Los Angeles, CA",
-    "latitude": 34.052235,
-    "longitude": -118.243683
-  },
-  {
-    "city": "Singapore",
-    "latitude": 1.290270,
-    "longitude": 103.851959
-  },
-  {
-    "city": "Tokyo",
-    "latitude": 35.6895,
-    "longitude": 139.6917
-  },
-  {
-    "city": "Hillsboro, OR",
-    "latitude": 45.5229,
-    "longitude": -122.9898
-  },
-  {
-    "city": "Vine Hill, Virginia",
-    "latitude": 37.9940,
-    "longitude": -77.4291
-  },
-  {
-    "city": "Buffalo, NY",
-    "latitude": 42.8864,
-    "longitude": -78.8784
-  },
-  {
-    "city": "Dallas, TX",
-    "latitude": 32.7767,
-    "longitude": -96.7970
-  }
-];
 	// Add cities as dots on the map
-	function addCity(latitude: number, longitude: number) {
-		console.log('addCity', latitude, longitude)
+	function addCity(latitude: number, longitude: number, cityName: string, cityCode: string) {
+		console.log('addCity', latitude, longitude, cityName)
 		const cityFeature = new Feature({
 			geometry: new Point(fromLonLat([longitude, latitude])),
 		})
 
-		cityFeature.setStyle(
-			new Style({
-				image: new Icon({
-					// Specify your dot icon here
-					//src: '/icon-16.png',
-					src: '/icon.svg',
-					height: 16,
-					width: 16,
-				}),
-			})
-		)
-
+		cityFeature.setStyle(new Style({
+			image: new CircleStyle({
+			radius: 8, // Adjust the radius as needed
+			fill: new Fill({
+				color: '#00ff00', // green fill color
+			}),
+			stroke: new Stroke({
+				color: '#000000', // black stroke color
+				width: 1, // Adjust stroke width as needed
+			}),
+			}),
+		}));		
+		// cityFeature.setStyle(new Style({
+		// 	image: new Icon({
+		// 	src: '/circle.svg',
+		// 	color: '#ff0000',
+		// 	// scale: 0.05 // Adjust the scale as needed
+		// 	height: 16,
+		// 	width: 16,
+		// 	})
+		// }));
+		cityFeature.set('name', cityName);
+		cityFeature.set('cityCode', cityCode);
+		
 		vectorSource.addFeature(cityFeature)
-		return
-		//   const cityFeature = new Feature({
-		//     geometry: new Point([longitude, latitude]),
-		//   });
-
-		//   cityFeature.setStyle(new Style({
-		//     image: new Icon({
-		//       // Specify your dot icon here
-		//       src: '/icon-64.png',
-		//     }),
-		//   }));
-
-		//   console.log('map', map)
-		//   map.getSource().addFeature(cityFeature);
 	}
+	// Create a tooltip element
+	const tooltipElement = document.createElement('div');
+	tooltipElement.className = 'tooltip'; // Make sure to define this class in your CSS
+
+	// Create an overlay for the tooltip
+	const tooltipOverlay = new Overlay({
+	element: tooltipElement,
+	positioning: 'bottom-center',
+	offset: [0, -10],
+	stopEvent: false
+	});
 
 	const ionViewDidEnter = async () => {
 		console.log('ionViewDidEnter')
-		initializeMap()
-        for (const city of cities) {
-            addCity(city.latitude, city.longitude)
-        }
+		await initializeMap()
+		for (const city of cities) {
+			console.log('city', city)
+			console.log('city.metadata', city.metadata)
+			console.log('city.metadata.latitude', city.metadata?.latitude)
+			console.log('city.metadata.longitude', city.metadata?.longitude)
+			if (city.metadata?.latitude && city.metadata?.longitude) {
+				addCity(city.metadata.latitude, city.metadata.longitude, city.metadata?.city || '', city.code || '')
+			}
+		}
 		// const { data, error } = await pb.send('/get-environment',{"method":"GET"})
 		// console.log('enviroment:', data)
 	}
