a62fd7e033fc872cab4be0fe38268ec235251a39
Mark Burggraf <markb@mantisbible.com>
2024-01-29 09:04:05 -0800

    fixed mobile oauth signin

--- src/components/LoginProviderSignInButton.ts
+++ src/components/LoginProviderSignInButton.ts
@@ -2,7 +2,22 @@ import { pb } from '$services/backend.service';
 export const signInWithProvider = async (provider: string) => {
   // If the popup is being blocked on Safari, you can try the suggestion from:
   // https://github.com/pocketbase/pocketbase/discussions/2429#discussioncomment-5943061.
-  return await pb.collection('users').authWithOAuth2({ provider });
+
+  let w = window.open()
+  if (w) {
+    const retval = await pb.collection("users")
+    .authWithOAuth2({
+        provider: provider,
+        urlCallback: (url) => {
+          if (w) w.location.href = url
+        },
+      })    
+      return retval;
+  } else {
+    return null;
+  }
+
+  // return await pb.collection('users').authWithOAuth2({ provider });
 
   // custom scopes to overwrite the default ones
   // scopes?: Array<string>;
