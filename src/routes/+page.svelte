d89f0f0fa5871bd5f9acad0929ed3e3b95a76a32
Mark Burggraf <markb@mantisbible.com>
2024-01-29 09:50:55 -0800

    main page formatting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,213 +1,215 @@
 <script lang="ts">
-	import IonPage from "$ionpage";
+	import IonPage from '$ionpage'
 	import '$styles/grid-styles.css'
 	import * as allIonicIcons from 'ionicons/icons'
 
-	import { currentUser } from '$services/backend.service';
-	import { goto } from '$app/navigation';
+	import { currentUser } from '$services/backend.service'
+	import { goto } from '$app/navigation'
 	// import * as allIonicIcons from 'ionicons/icons';
-	import LoginModal from '$components/LoginModal.svelte';
-	import { modalController } from '$ionic/svelte';
+	import LoginModal from '$components/LoginModal.svelte'
+	import { modalController } from '$ionic/svelte'
 	// import { page } from '$app/stores';
 	import { onMount } from 'svelte'
-	import { currentState } from "$services/state.service";
+	import { currentState } from '$services/state.service'
 
 	//export let providers: Provider[] = [];
-	export const providers: string[] = [];
-	export const onSignIn: Function = () => {};
+	export const providers: string[] = []
+	export const onSignIn: Function = () => {}
 
-	const app_version = __APP_VERSION__;
-	const app_name = __APP_NAME__;
+	const app_version = __APP_VERSION__
+	const app_name = __APP_NAME__
 
 	onMount(() => {
-		const page = localStorage.getItem('page');
+		const page = localStorage.getItem('page')
 		// remove page from localStorage
 		if (page) {
-			localStorage.removeItem('page');
-			goto(page);
+			localStorage.removeItem('page')
+			goto(page)
 		}
 		return () => {}
 		if ($currentUser && $currentState.selectedMenuItem) {
 			console.log('**********************************')
 			console.log('$currentState.selectedMenuItem', $currentState.selectedMenuItem)
-			console.log('going to', $currentState.selectedMenuItem);
+			console.log('going to', $currentState.selectedMenuItem)
 			console.log('**********************************')
 			//goto($currentState.selectedMenuItem);
 		}
 		return () => {}
-	});
+	})
 
 	const openLoginBox = async () => {
 		const openLoginModalController = await modalController.create({
 			component: LoginModal,
 			componentProps: {
 				providers: ['google', 'github'],
-				onSignIn: () => { goto('/projects') },
+				onSignIn: () => {
+					goto('/projects')
+				},
 			},
 			showBackdrop: true,
 			backdropDismiss: true,
-		});
+		})
 
-		await openLoginModalController.present();
-	};
+		await openLoginModalController.present()
+	}
 </script>
+
 <IonPage>
-
-
-
-
-
 	<ion-header>
 		{#if $currentUser}
-		<ion-toolbar color="secondary">
-			<ion-buttons slot="start">
+			<ion-toolbar color="secondary">
+				<ion-buttons slot="start">
 					<ion-menu-button />
-			</ion-buttons>
-			<ion-title>Welcome</ion-title>
-		</ion-toolbar>
+				</ion-buttons>
+				<ion-title>Welcome</ion-title>
+			</ion-toolbar>
 		{/if}
 	</ion-header>
 	<ion-content class="ion-padding">
 		<div class="width-400">
 			{#if $currentUser}
-			<div class="ion-text-center" style="margin-top: -30px;">
-				<ion-grid>
-					<ion-row>
-						<ion-col size="auto">
-							<ion-img style="height: 40px; width:40px; margin-left:10px; margin-right: 10px; margin-top: 30px;" src="/icon.svg"></ion-img>
-						</ion-col>
-						<ion-col size="auto">
-							<h1>Azabab</h1>
-							<p>Hosting Service</p>			
-						</ion-col>
-					</ion-row>
-				</ion-grid>
-			</div>
+				<div class="ion-text-center" style="display: flex; align-items: center; justify-content: center; margin-top: -30px; margin-bottom: 0px;">
+					<ion-img
+						style="height: 65px; width: 65px; margin-right: 10px; margin-top: 0px;"
+						src="/icon.svg"
+					/>
+					<div>
+						<h1 style="font-size: 30pt;"><b>Azabab</b></h1>
+						<p style="margin-top: -5px;">High Availability Made Simple</p>
+					</div>
+				</div>			
 				<div class="center">
 					Welcome back <b>{$currentUser?.name || $currentUser?.email}</b>
-				</div><br/>
-				<ion-button on:click={()=>{goto('/newproject')}} color="secondary" size="large" expand="block">
-					<ion-icon slot="start" icon={allIonicIcons.documentOutline}></ion-icon>
+				</div>
+				<br />
+				<ion-button
+					on:click={() => {
+						goto('/newproject')
+					}}
+					color="secondary"
+					size="large"
+					expand="block"
+				>
+					<ion-icon slot="start" icon={allIonicIcons.documentOutline} />
 					Create a project now.
-				</ion-button><br/>
-				<ion-button on:click={()=>{goto('/projects')}} color="primary" size="large" expand="block">
-					<ion-icon slot="start" icon={allIonicIcons.appsOutline}></ion-icon>
+				</ion-button><br />
+				<ion-button
+					on:click={() => {
+						goto('/projects')
+					}}
+					color="primary"
+					size="large"
+					expand="block"
+				>
+					<ion-icon slot="start" icon={allIonicIcons.appsOutline} />
 					View your list of projects.
-				</ion-button><br/>
-		{:else}
-			
-			<div class="ion-text-center">
-				<ion-grid>
-					<ion-row>
-						<ion-col size="auto">
-							<ion-img style="height: 60px; width:60px; margin-left:10px; margin-right: 10px; margin-top: 30px;" src="icon.svg"></ion-img>
-						</ion-col>
-						<ion-col size="auto">
-							<h1><b>Welcome to Azabab</b></h1>
-							<p>Hosting Made Easy</p>			
-						</ion-col>
-					</ion-row>
-				</ion-grid>
+				</ion-button><br />
+			{:else}
+				<div class="ion-text-center" style="display: flex; align-items: center; justify-content: center; margin-top: -30px; margin-bottom: 0px;">
+					<ion-img
+						style="height: 65px; width: 65px; margin-right: 10px; margin-top: 0px;"
+						src="/icon.svg"
+					/>
+					<div>
+						<h1 style="font-size: 30pt;"><b>Azabab</b></h1>
+						<p style="margin-top: -5px;">High Availability Made Simple</p>
+					</div>
+				</div>			
+			{/if}
+		</div>
+		{#if !$currentUser}
+			<div class="width-400">
+				<ion-button expand="block" on:click={openLoginBox}>Get Started</ion-button>
 			</div>
 		{/if}
-	</div>
-	<div class="width-400">
-		<ion-button expand="block" on:click={openLoginBox}>Get Started</ion-button>
-	</div>
-	<div class="flex-container">
-		<div class="flex-item secondary">
-			<h3>What is Azabab?</h3>
-			<ul>
-				<li>Pocketbase Hosting in 5 seconds</li>
-				<li>High Availabilty</li>
-				<li>Globally Distributed</li>
-				<li>Read-Only or Read-Write Replicas</li>
-				<li>Easy Management Tools</li>
-			</ul>
-		</div>
-		<div class="flex-item tertiary">
-			<h3>Highly Available Pocketbase</h3>
-			<ul>
-				<li>Setup Globally Distributed Replicas</li>
-				<li>Automatic Replication Setup</li>
-				<li>Failover Handling</li>
-				<li>Direct Access to Regional Instances</li>
-				<li>Streaming Backups to S3 (with PITR)</li>
-			</ul>			
-		</div>
-		<div class="flex-item primary">
-			<h3>Standard Features</h3>
-			<ul>
-				<li>Create & Manage Projects</li>
-				<li>Create & Manage Replicas</li>
-				<li>Full Pocketbase Admin Access</li>
-				<li>Clone Projects (Prod/Staging/Dev)</li>
-				<li>Online Management Tools</li>
-			</ul>
-		</div>
-		<div class="flex-item secondary">
-			<h3>Advanced Features</h3>
-			<ul>
-				<li>Manage Instance Status</li>
-				<li>View/Edit Project Files</li>
-				<li>Deploy Files with ssh/scp, sftp</li>
-				<li>Pocketbase Logs</li>
-				<li>Site Metrics: cpu, memory, disk, network</li>
-			</ul>
-		</div>
-		<div class="flex-item tertiary">
-			<h3>Pocketbase Features</h3>
-			<ul>
-				<li>Database with Automatic API</li>
-				<li>Authentication (email, oauth, social)</li>
-				<li>Storage with S3 Support</li>
-				<li>Realtime Database</li>
-				<li>Management Dashboard, Hooks, & more</li>
-			</ul>
-		</div>
-		<!-- <div class="flex-item tertiary">
+		<div class="flex-container">
+			<div class="flex-item secondary">
+				<h3>What is Azabab?</h3>
+				<ul>
+					<li>Pocketbase Hosting in 5 seconds</li>
+					<li>High Availabilty</li>
+					<li>Globally Distributed</li>
+					<li>Read-Only or Read-Write Replicas</li>
+					<li>Easy Management Tools</li>
+				</ul>
+			</div>
+			<div class="flex-item tertiary">
+				<h3>Highly Available Pocketbase</h3>
+				<ul>
+					<li>Setup Globally Distributed Replicas</li>
+					<li>Automatic Replication Setup</li>
+					<li>Failover Handling</li>
+					<li>Direct Access to Regional Instances</li>
+					<li>Streaming Backups to S3 (with PITR)</li>
+				</ul>
+			</div>
+			<div class="flex-item primary">
+				<h3>Standard Features</h3>
+				<ul>
+					<li>Create & Manage Projects</li>
+					<li>Create & Manage Replicas</li>
+					<li>Full Pocketbase Admin Access</li>
+					<li>Clone Projects (Prod/Staging/Dev)</li>
+					<li>Online Management Tools</li>
+				</ul>
+			</div>
+			<div class="flex-item secondary">
+				<h3>Advanced Features</h3>
+				<ul>
+					<li>Manage Instance Status</li>
+					<li>View/Edit Project Files</li>
+					<li>Deploy Files with ssh/scp, sftp</li>
+					<li>Pocketbase Logs</li>
+					<li>Site Metrics: cpu, memory, disk, network</li>
+				</ul>
+			</div>
+			<div class="flex-item tertiary">
+				<h3>Pocketbase Features</h3>
+				<ul>
+					<li>Database with Automatic API</li>
+					<li>Authentication (email, oauth, social)</li>
+					<li>Storage with S3 Support</li>
+					<li>Realtime Database</li>
+					<li>Management Dashboard, Hooks, & more</li>
+				</ul>
+			</div>
+			<!-- <div class="flex-item tertiary">
 			<h3>What else?</h3>
 			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius id cupiditate exercitationem itaque modi sequi debitis quidem ratione minus placeat beatae quos laudantium iure consequatur possimus rerum tempore, dolore in.</p>
 		</div> -->
-	</div>
-	<!-- <br/> -->
-	<!-- <br/><br/><br/><br/>&nbsp; -->
+		</div>
+		<!-- <br/> -->
+		<!-- <br/><br/><br/><br/>&nbsp; -->
 	</ion-content>
 	<ion-footer>
 		{#if !$currentUser}
-		<ion-toolbar color="transparent" class="ion-text-center">
-			<b>{app_name} {app_version}</b><br/>
-			<span
-				on:click={() => {
-					goto("/terms");
-				}}
-				class="pointer">Terms of Service</span
-			>
-			<span>&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;</span>
-			<span
-				on:click={() => {
-					goto("/privacy");
-				}}
-				class="pointer">Privacy</span
-			>
-			<span>&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;</span>
-			<span
-				on:click={() => {
-					goto("/support");
-				}}
-				class="pointer">Support</span
-			>
-		</ion-toolbar>
+			<ion-toolbar color="transparent" class="ion-text-center">
+				<b>{app_name} {app_version}</b><br />
+				<span
+					on:click={() => {
+						goto('/terms')
+					}}
+					class="pointer">Terms of Service</span
+				>
+				<span>&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;</span>
+				<span
+					on:click={() => {
+						goto('/privacy')
+					}}
+					class="pointer">Privacy</span
+				>
+				<span>&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;</span>
+				<span
+					on:click={() => {
+						goto('/support')
+					}}
+					class="pointer">Support</span
+				>
+			</ion-toolbar>
 		{/if}
 	</ion-footer>
 
-
-
-
-
-
-
-<!-- <ion-header>
+	<!-- <ion-header>
 	{#if $currentUser}
 		<ion-toolbar color="primary">
 			<ion-buttons slot="start">
