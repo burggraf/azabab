9ea372ee3aa1c1d8c1d036ee7b7cc757136432b3
Mark Burggraf <markb@mantisbible.com>
2024-01-31 09:08:15 -0800

    remove console logs

--- src/routes/instance/[id]/+page.svelte
+++ src/routes/instance/[id]/+page.svelte
@@ -56,29 +56,29 @@
 	let initTab: string;
 	onMount(async () => {
 		initTab = localStorage.getItem('instance.tab') || 'settings'
-		console.log('localStorage got ====> initTab', initTab)
+		// console.log('localStorage got ====> initTab', initTab)
 		setTimeout(() => {
 			const tb: any = document.getElementById('ion-tabs')
-			console.log('setTimeout changing tab to:', initTab)
-			console.log('tb is ', tb)
+			// console.log('setTimeout changing tab to:', initTab)
+			// console.log('tb is ', tb)
 			if (tb) tb.select(initTab || 'settings')
 		}, 100)
 		localStorage.setItem('page', window.location.pathname)
 		
 	})
 	const ionViewWillEnter = async () => {
-		console.log('*** ionViewWillEnter, id', id)
+		// console.log('*** ionViewWillEnter, id', id)
 		if (!$currentUser) {
 			goto('/');
 		}
 		
-		console.log('*** looking for instances with id', id)
+		// console.log('*** looking for instances with id', id)
 		project_instance = await pb.collection('instance_view').getFirstListItem(
 			`id = "${id}"`,{
 			fields: 'name, project_id, owner, ownertype, code, domain, id, port, site_domain, site_name, site_id, type, db_streaming_backup_location, logs_streaming_backup_location, db_streaming_backup_retention, logs_streaming_backup_retention, instance_status',
 		})
 
-		console.log('*****  project_instance', project_instance)
+		// console.log('*****  project_instance', project_instance)
 		sites = await pb.collection('sites').getFullList({
 			fields: 'id, name, code, domain, active',
 		})
@@ -109,9 +109,9 @@
 		window.open(`https://${project_instance.domain}.${project_instance.site_domain}/`, '_blank')
 	}
 	const removeinstance = async () => {
-		console.log('removeinstance')
-		console.log('project_instance', project_instance)
-		console.log('sites', sites)
+		// console.log('removeinstance')
+		// console.log('project_instance', project_instance)
+		// console.log('sites', sites)
 		if (projectInstances.length > 1 && project_instance.type === 'primary') {
 			toast('You cannot remove the primary instance if there are existing replicas', 'danger')
 			return
@@ -125,7 +125,7 @@
 			header: 'Remove Project Instance',
 			message: `Are you SURE?  This cannot be undone.  If this is the last instance, the project will also be removed.`,
 			okHandler: async () => {
-				console.log('calling /removeproject')
+				// console.log('calling /removeproject')
 				const { data, error } = await pb.send('/removeproject', {
 					method: 'POST',
 					body: {
@@ -136,23 +136,21 @@
 				// we need to call setup-marmot to reconfigure marmot
 				// this will delete marmot if there is only one instance left
 				// it will also remove sync files if there is only one instance left
-				console.log('********************');
-				console.log(`/setup-marmot/${project_instance.project_id}`)
-				console.log('********************');
+				// console.log('********************');
+				// console.log(`/setup-marmot/${project_instance.project_id}`)
+				// console.log('********************');
 				const { data:setupMarmotData, error:setupMarmotError } = 
 					await pb.send(`/setup-marmot/${project_instance.project_id}`, {
 					method: 'GET',
 				})
 				if (setupMarmotError) {
-					console.log('/setup-marmot error', setupMarmotError)
+					console.error('/setup-marmot error', setupMarmotError)
 					toast(JSON.stringify(setupMarmotError), 'danger')
-				} else {
-					console.log('/setup-marmot data', setupMarmotData)
 				}
 
-				console.log('data', data)
-				console.log('typeof data', typeof data)
-				console.log('error', error)
+				// console.log('data', data)
+				// console.log('typeof data', typeof data)
+				// console.log('error', error)
 				if (error) {
 					toast('Error: ' + JSON.stringify(error), 'danger')
 				} else {
@@ -257,7 +255,7 @@
 				text: 'Cancel',
 				icon: allIonicIcons.closeOutline,
 				handler: () => {
-					console.log('Cancel clicked')
+					// console.log('Cancel clicked')
 				},
 			},
 		]
@@ -300,15 +298,15 @@
 	<ion-content class="ion-padding">
 		<ion-tabs id="ion-tabs"
 			on:ionTabsDidChange={(e) => {
-				console.log('*********************************************')
-				console.log('***** ionTabsDidChange               ********')
-				console.log('*********************************************')
-				console.log('e', e)
-				console.log(e.detail.tab);
-				console.log('loccalStorage setting instance.tab', e.detail.tab)
+				// console.log('*********************************************')
+				// console.log('***** ionTabsDidChange               ********')
+				// console.log('*********************************************')
+				// console.log('e', e)
+				// console.log(e.detail.tab);
+				// console.log('loccalStorage setting instance.tab', e.detail.tab)
 				localStorage.setItem('instance.tab', e.detail.tab)
 				instanceTab.set(e.detail.tab)
-				console.log('*********************************************')
+				// console.log('*********************************************')
 			}}
 		>
 			<ion-tab tab="settings">
