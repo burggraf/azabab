2f9d5dd337939bd91ba954120c923b66513e5b67
Mark Burggraf <markb@mantisbible.com>
2024-02-02 11:25:17 -0800

    added dns_host flag

---
+++ pb_migrations/1706901841_updated_sites.js
@@ -0,0 +1,27 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("souws1inx11patr")
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "2avvwwfk",
+    "name": "dns_host",
+    "type": "bool",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {}
+  }))
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("souws1inx11patr")
+
+  // remove
+  collection.schema.removeField("2avvwwfk")
+
+  return dao.saveCollection(collection)
+})
